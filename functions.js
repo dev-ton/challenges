1bcaad35541d37ef355c6ede564e2a9e67076cb9
Antonin Jech <a.jech@audaris.de>
2023-03-30 09:31:16 +0200

    project made

--- functions.js
+++ functions.js
@@ -1,14 +1,18 @@
 // Challenge #1 - returns second largest number
+
 const secondLargest = (arr) => {
     if (!arr.some(isNaN)) {
     const sorted = arr.sort((a, b) => b - a);
-    return console.log(sorted.at(1));
+    return console.log(`Second largest number is: ${sorted.at(1)}`);
     }
-    return console.log('Please provide array of numbers.')
+    return console.warn('Please provide array of numbers.')
   };
   
   const array = [246, 16, 61584, 547, 1, 3, 99, 48, 1465, 65, 2023];
-  secondLargest(array);
+  console.group();
+    console.log("CHALLENGE #1");
+    secondLargest(array);
+  console.groupEnd();
   
   // Challenge #2 - returns letter from alphabet
   const letterAtPosition = (position) => {
@@ -16,15 +20,15 @@ const secondLargest = (arr) => {
     if (charDecimal > 1 && charDecimal <= 122) {
       console.log(`Your letter is: ${String.fromCharCode(charDecimal)}`);
     } else {
-      console.log("invalid");
+      console.warn("invalid");
     }
   };
-  letterAtPosition(21);
+  console.group();
+    console.log("CHALLENGE #2");
+    letterAtPosition(21);
+  console.groupEnd();
   
   // Challenge #3 - pair occurences
-  const str1 = "";
-  const str2 = "";
-  
   const findCommonSubstring = (str1, str2) => {
     let count = 0
     if (str1 && str2) {
@@ -40,7 +44,14 @@ const secondLargest = (arr) => {
   
       return console.log(`Number of pair occurences between 2 strings: ${count}`)
     } 
-      return count
+      return console.warn('No common occurences found')
   };
+
+  const str1 = "";
+  const str2 = "";
+  console.group();
+    console.log("CHALLENGE #3");
+    findCommonSubstring(str1, str2)
+  console.groupEnd();
   
   
